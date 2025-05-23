01640cc48425c158b9ee5ffdb4532d898dc1fece
Jericho Serrano <118679068+jericho1050@users.noreply.github.com>
2025-05-24 07:44:21 +0800

    test: add media store unit tests

--- src/test/mocks/handlers.ts
+++ src/test/mocks/handlers.ts
@@ -1,10 +1,6 @@
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
-
-export const handlers = [
-  rest.get('/health', (_req, res, ctx) =>
-    res(ctx.status(200), ctx.json({ status: 'ok' })),
-  ),
-]
-
-export const server = setupServer(...handlers)
+export const handlers = []
+export const server = {
+listen: () => {},
+resetHandlers: () => {},
+close: () => {},
+}
